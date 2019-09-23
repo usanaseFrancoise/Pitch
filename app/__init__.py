d1436478ce7444d2b855bffa9462f9906537c2c5
usanase Francoise <fusanasencoise@gmail.com>
2019-09-23 13:06:00 +0200

    add __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,9 @@ def create_app(config_name):
 
     login_manager.init_app(app)
     db.init_app(app)
-    
+    bootstrap.init_app(app)
+    configure_uploads(app,photos)
+    mail.init_app(app)
     # simple.init_app(app)
     
     return app
