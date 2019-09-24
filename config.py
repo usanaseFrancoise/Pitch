bb7be3c3c9752192a4c65f864ea6765f821032e0
usanase Francoise <fusanasencoise@gmail.com>
2019-09-24 09:18:20 +0200

    add postgress

--- config.py
+++ config.py
@@ -15,22 +15,21 @@ class Config:
      # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fanny:havugima@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://fanny:havugima@localhost/pitches"
 
 
 class ProdConfig(Config):
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fanny:havugima@localhost/pitches'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-# class DevConfig(Config):
-#     DEBUG = True
+
 
 class TestConfig(Config):
 
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fanny:havugima@localhost/pitchestest'
 
+    
 class DevConfig(Config):
-
-    DEBUG = True
+    DEBUG = True 
 
 config_options = {
 'development':DevConfig,
